78bcc0dc6101579d71c06644c0bcbd309f74b5fd
Nikhil kumar <96740599+Nikhil-kumar-1@users.noreply.github.com>
2025-09-19 17:08:05 +0530

    Update config.js

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,2 +1,2 @@
-// export const backendUrl="https://namsropa.onrender.com"
-export const backendUrl="http://localhost:5000"
+ export const backendUrl="https://namsropa.onrender.com"
+//export const backendUrl="http://localhost:5000"
